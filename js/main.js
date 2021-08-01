8598fc3380958ba6f6d68e390436c840855ccdf5
Viktor-stack <grohik493@gmail.com>
2021-08-01 17:19:03 +0300

    added mobile requests

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(function () {
     $('select').styler();
+
+    $('.menu-btn').on('click', function (e) {
+        e.preventDefault();
+        $(".one").toggleClass('active')
+        $(".three").toggleClass('active')
+        $(".two").toggleClass('hidden')
+        $('.menu').toggleClass('menu_active');
+        $('.content').toggleClass('content_active');
+    })
+
 })
